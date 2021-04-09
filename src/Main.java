326b093293a541296eb27dba24b8bd48c54ae06a
Neha Dayma <neha.dayma@hm.com>
2021-04-09 16:02:14 +0200

    To Lower Case

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,19 @@ public class Main {
 	    List<String> stringList = List.of("Avatar, Captain Philips,The Avengers");
 
 	    List<String> sortedList = sortList(stringList);
+
+        List<String> toLowercase = toLowerCase(sortedList);
     }
 
     private static List<String> sortList(List<String> movieList) {
         return movieList.stream()
                 .sorted()
+                .collect(Collectors.toList());
+    }
+
+    private static List<String> toLowerCase(List<String> movieList) {
+        return movieList.stream()
+                .map(s -> s.toLowerCase())
                 .collect(Collectors.toList());
     }
 }
